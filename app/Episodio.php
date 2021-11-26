f4ee23b1940e80103a5255e85deab107f5975bd6
Ricardo Altmann <ricardoaltmann@outlook.com.br>
2021-11-26 08:56:42 -0300

    first commit

---
+++ app/Episodio.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Episodio extends Model
+{
+    public $timestamps = false;
+    protected $fillable = ['temporada', 'numero', 'assistido', 'serie_id'];
+
+    public function serie()
+    {
+        return $this->belongsTo(Serie::class);
+    }
+
+    public function getAssistidoAttribute ($assistido): bool
+    {
+        return $assistido;
+    }
+}
